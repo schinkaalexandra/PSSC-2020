bb112eac8bba6189507f7972e4c3789e40fa4fee
schinkaalexandra <71776528+schinkaalexandra@users.noreply.github.com>
2020-10-20 22:32:42 +0300

    Tema 4

---
+++ Schinka_Alexandra/L04/Question.Domain/NewQuestionWorkflow/NewQuestionCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace Question.Domain.NewQuestionWorkflow
+{
+    public struct NewQuestionCmd
+    {
+        [Required]                                      //denumirile sunt aceleasi cu cele de pe site si in aceeasi ordine: title, body, tags
+        public string Title { get; private set; }           //titlul intrebarii care se vrea a fi pusa
+        [Required]
+        public string Body { get; private set; }            //descrierea succinta dar clara a intrebarii, portiuni de cod, erori, metode incercate
+        public string Tags { get; set; }                     //tag urile (minimum 1, maximum 5 tag- uri per intrebare)
+        public NewQuestionCmd(string title, string body, string tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+    }
+}
---
+++ Schinka_Alexandra/L04/Question.Domain/NewQuestionWorkflow/NewQuestionResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using CSharp.Choices;
+using System.Linq;
+
+namespace Question.Domain.NewQuestionWorkflow
+{
+    [AsChoice]
+    public static partial class NewQuestionResult
+    {
+        public interface PostNewQuestionResult { }
+        public class QuestionPosted : PostNewQuestionResult    //intrebare validata si postata
+        {
+            public Guid Id { get; private set; }
+            public string Title { get; private set; }
+            public string Body { get; private set; }
+            public string Tags { get; private set; }
+            public QuestionPosted(Guid id, string title, string body, string tags)
+            {
+                Id = id;
+                Title = title;
+                Body = body;
+                Tags = tags;
+            }
+        }
+        public class QuestionNotPosted : PostNewQuestionResult             //intrebare nevalidata si nepostata
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotPosted(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        public class QuestionValidationFailed : PostNewQuestionResult        //intrebare nevalidata
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+}
