27343b6e0d01e6c6001667c311ccfc2c28615629
schinkaalexandra <71776528+schinkaalexandra@users.noreply.github.com>
2021-01-04 14:41:49 +0200

    Add files via upload

---
+++ Schinka_Alexandra/Proiect/Samples/StackUnderflow.EF.Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackUnderflow.DatabaseModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackUnderflow.EF
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+
+        }
+          public DbSet<Questions> Questions { get; set; }
+        
+    }
+}
---
+++ Schinka_Alexandra/Proiect/Samples/StackUnderflow.Schema/Models/Questions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace StackUnderflow.DatabaseModel.Models
+{
+    [Table("Question")]
+    public class Questions
+    {
+        [Key]
+        public Guid QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Tags { get; set; }
+    }
+}
