c14c0dcf32471740f6ee49931832c626fa84b852
schinkaalexandra <71776528+schinkaalexandra@users.noreply.github.com>
2021-01-04 14:30:29 +0200

    Add files via upload

---
+++ Schinka_Alexandra/Proiect/Grains/AsyncHelloGrain.cs
@@ -0,0 +1,47 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Streams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class AsyncHelloGrain : Orleans.Grain, IAsyncHello, IAsyncObserver<string>
+    {
+        private readonly ILogger logger;
+
+        public AsyncHelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async override Task OnActivateAsync()
+        {
+            IAsyncStream<string> stream = this.GetStreamProvider("SMSProvider").GetStream<string>(Guid.Empty, "chat");
+            await stream.SubscribeAsync(this);
+        }
+
+        public async Task OnNextAsync(string item, StreamSequenceToken token = null)
+        {
+            Console.WriteLine($"{this.GetPrimaryKeyString()} - {item}");
+        }
+
+        public Task OnCompletedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnErrorAsync(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
